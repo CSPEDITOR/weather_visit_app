337b41f3fc7611d050c4efb0ade9a22b4d607e91
Chandra shekhara prasad <chandrashekharaprasad@gmail.com>
2025-03-18 12:21:45 +0530

    adding a feature which is no place exist

--- weather_visit_app/src/SearchBox.jsx
+++ weather_visit_app/src/SearchBox.jsx
@@ -5,37 +5,48 @@ import { useState } from "react";
 
 export default function SearchBox({ updateInfo }) {
   let [city, setCity] = useState("");
+  let [error, setError] = useState(false);
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
   const API_KEY = "25bcdcac4edc12957478bca8182d329b";
 
   let getWeatherInfo = async () => {
-    let response = await fetch(
-      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
-    );
-    let jsonResponse = await response.json();
-    console.log(jsonResponse);
-    let result = {
-      city: jsonResponse.name,
-      temp: jsonResponse.main.temp,
-      tempMin: jsonResponse.main.temp_min,
-      tempMax: jsonResponse.main.temp_max,
-      humidity: jsonResponse.main.humidity,
-      feelsLike: jsonResponse.main.feelsLike,
-      weather: jsonResponse.weather[0].description,
-    };
-    console.log(result);
-    return result;
+    try {
+      let response = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      let jsonResponse = await response.json();
+      console.log(jsonResponse);
+      let result = {
+        city: jsonResponse.name,
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feelsLike,
+        weather: jsonResponse.weather[0].description,
+      };
+      console.log(result);
+      return result;
+    } catch (error) {
+     throw error;
+    }
+    
   };
 
   let handleChange = (e) => {
     setCity(e.target.value);
   };
-  let handlesubmit = async(e) => {
-    e.preventDefault();
-    console.log(city);
-    setCity("");
-    let newInfo =await getWeatherInfo();
-    updateInfo(newInfo);
+  let handlesubmit = async (e) => {
+    try {
+      e.preventDefault();
+      console.log(city);
+      setCity("");
+      let newInfo = await getWeatherInfo();
+      updateInfo(newInfo);
+      
+    } catch (error) {
+      setError(true);
+    }
   };
   return (
     <div className="searchBox">
@@ -52,6 +63,7 @@ export default function SearchBox({ updateInfo }) {
         <Button variant="contained" type="submit">
           Search
         </Button>
+        {error && <p style={{color :"red"}}>NO such place exist</p>}
       </form>
     </div>
   );
